82bc150bfac2c18debb960a855e366f5bba1d14c
Francis Peter <115923152+Francis-Yuppie@users.noreply.github.com>
2024-01-29 12:26:40 +0300

    Update anime.ts

--- app/ui/anime.ts
+++ app/ui/anime.ts
@@ -1,32 +1,32 @@
 // animate typing
 
-export const anime = {
-	textTyping(text, index) {
-	if(index > text.length){
-		document.querySelector('#out').textContent += text[index];
-		index++
+// export const anime = {
+// 	textTyping(text, index) {
+// 	if(index > text.length){
+// 		document.querySelector('#out').textContent += text[index];
+// 		index++
 
-		setTimeout( () => {
-             textTyping(text, index)
-		}, Math.floor(Math.random() * 200) + 100)
-	} else {
-		setTimeout( () => {
-			document.querySelector('#out').textContent = '';
-			textTyping(text, 0)
-		}, 2000)
-	}
-};
+// 		setTimeout( () => {
+//              textTyping(text, index)
+// 		}, Math.floor(Math.random() * 200) + 100)
+// 	} else {
+// 		setTimeout( () => {
+// 			document.querySelector('#out').textContent = '';
+// 			textTyping(text, 0)
+// 		}, 2000)
+// 	}
+// };
 
-document.addEventListener('DOMContentLoaded', () => {
-	textTyping('Hello', 0)
-})
-};
+// document.addEventListener('DOMContentLoaded', () => {
+// 	textTyping('Hello', 0)
+// })
+// };
 
 
-useEffect(() => {
+// useEffect(() => {
 
-    anime.textTyping('Hello', 0);
+//     anime.textTyping('Hello', 0);
     
-  }, []); 
+//   }, []); 
 
 
